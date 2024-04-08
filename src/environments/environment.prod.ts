d0a2ea85cae88cd94c4275da53e168bf144f7785
VishalKanaka <113039204+VishalKanaka@users.noreply.github.com>
2024-04-08 01:19:57 -0400

    a

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  production: true
+};
+
+
+export const SpotifyConfiguration = {
+  clientId: 'a9764733eb644856bca9906179c44358',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing", 
+    "user-read-recently-played",
+    "user-read-playback-state", 
+    "user-top-read",
+    "user-modify-playback-state", 
+    "user-library-read", 
+    "playlist-read-private", 
+    "playlist-read-collaborative" 
+  ]
+}
